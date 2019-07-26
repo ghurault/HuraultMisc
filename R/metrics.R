c1a144b89b923d4736086e7cf16ffdcd81519c1f
Guillem Hurault <39564817+ghurault@users.noreply.github.com>
2019-07-26 15:02:02 +0100

    Added calibration by LOWESS

--- R/metrics.R
+++ R/metrics.R
@@ -2,38 +2,93 @@
 
 #' Estimate calibration given forecasts and corresponding outcomes
 #'
-#' @param forecast Vector of probability forecast
+#' @param forecast Vector of probability forecasts
 #' @param outcome Vector of observations (0 or 1)
-#' @param res Resolution (bin's width). If NULL, automatic bin width selection with Sturges' method
+#' @param method Moethod used to estimate calibration, either "smoothing" or "binning"
+#' @param CI Confidence level (e.g. 0.95). CI not computed if NULL (CI can be expensive to compute for LOWESS).
+#' @param span LOWESS span when calibration is estimated by smoothing
+#' @param binwidth Binwidth when calibration is estimated by binning.  If NULL, automatic bin width selection with Sturges' method.
 #'
-#' @return Dataframe with colums Forecast (bins), Frequency (frequency of outcomes in the bin), Lower (lower bound of the 95% CI) and Upper (upper bound of the 95% CI)
+#' @return Dataframe with colums Forecast (bins), Frequency (frequency of outcomes in the bin), Lower (lower bound of the CI) and Upper (upper bound of the CI)
 #' @export
 #'
 #' @examples
-#' N <- 10000
-#' f <- runif(N)
-#' o <- sapply(1:N, function(i) {rbinom(1, 1, f[i])})
-#' cal <- compute_calibration(f, o)
-#' with(cal, plot(Forecast, Frequency))
-#' abline(c(0, 1), col = "red")
-compute_calibration <- function(forecast, outcome, res = NULL) {
+#' N <- 1e4
+#' f <- rbeta(N, 1, 1)
+#' o <- sapply(f, function(x) {rbinom(1, 1, x)})
+#' lapply(c("binning", "smoothing"),
+#'        function(m) {
+#'          cal <- compute_calibration(f, o, method = m)
+#'          with(cal, plot(Forecast, Frequency))
+#'          abline(c(0, 1), col = "red")
+#'        })
+compute_calibration <- function(forecast, outcome, method = "smoothing", CI = NULL , span = 0.7, binwidth = NULL) {
 
-  if (is.null(res)){res <- 1 / nclass.Sturges(as.vector(as.matrix(forecast)))} # Automatic bin width selection
+  if (!is.null(CI)){
+    if (CI >= 1 | CI <= 0)
+      stop("If not NULL, CI should be between 0 and 1")
+  }
 
-  forecast <- round(forecast / res) * res
-  bins <- seq(0, 1, res)
+  if (method == "smoothing") {
 
-  count_f <- table(factor(forecast, levels = bins)) # Number of forecast in each bin
-  count_a <- table(factor(forecast[outcome == 1], levels = bins)) # Number of outcomes in each bin
+    fit <- loess(outcome ~ forecast, span = span)
 
-  out <- Hmisc::binconf(count_a, count_f, alpha = 0.05, method = "exact")
-  out <- as.data.frame(out)
-  colnames(out)[1] <- "Frequency"
-  out <- cbind(data.frame(Forecast = bins), out)
-  rownames(out) <- NULL
+    out <- data.frame(Forecast = seq(0, 1, .01))
+    if (!is.null(CI)) {
+      tmp <- predict(fit, newdata = out$Forecast, se = TRUE)
+      out$Frequency <- tmp$fit
+      t <- .5 + CI / 2
+      out$Lower <- with(tmp, pmax(0, fit - qt(t, df) * se.fit))
+      out$Upper <- with(tmp, pmin(1, fit + qt(t, df) * se.fit))
+      out <- rbind(data.frame(Forecast = 0, Frequency = 0, Lower = 0, Upper = 0),
+                   na.omit(out)) # Remove missing and add 0
+    } else {
+      tmp <- predict(fit, newdata = out$Forecast, se = FALSE)
+      out$Frequency <- tmp
+      out <- rbind(data.frame(Forecast = 0, Frequency = 0),
+                   na.omit(out)) # Remove missing and add 0
+    }
 
-  na.omit(out)
+  } else if (method == "binning") {
+
+    if (is.null(binwidth)) {
+      binwidth <- 1 / nclass.Sturges(as.vector(as.matrix(forecast))) # Automatic bin width selection
+    } else if (binwidth <= 0 | binwidth >= 1) {
+      stop("binwidth should be NULL or between 0 and 1")
+    }
+    forecast <- round(forecast / binwidth) * binwidth
+    bins <- seq(0, 1, binwidth)
+
+    count_f <- table(factor(forecast, levels = bins)) # Number of forecast in each bin
+    count_a <- table(factor(forecast[outcome == 1], levels = bins)) # Number of outcomes in each bin
+
+    if (!is.null(CI)){
+      out <- Hmisc::binconf(count_a, count_f, alpha = 1 - CI, method = "exact")
+      out <- as.data.frame(out)
+      colnames(out)[1] <- "Frequency"
+      out <- cbind(data.frame(Forecast = bins), out)
+      rownames(out) <- NULL
+      out <- na.omit(out)
+    } else {
+      out <- data.frame(Forecast = bins, Frequency = as.numeric(count_a / count_f))
+    }
+
+  } else {
+    stop("method should be either smoothing or binning")
+  }
+  return(out)
 }
+
+N <- 1e4
+f <- rbeta(N, 1, 1)
+o <- sapply(f, function(x) {rbinom(1, 1, x)})
+lapply(c("binning", "smoothing"),
+       function(m) {
+         cal <- compute_calibration(f, o, method = m)
+         with(cal, plot(Forecast, Frequency))
+         abline(c(0, 1), col = "red")
+       })
+
 
 # Compute resolution ------------------------------------------------------
 
