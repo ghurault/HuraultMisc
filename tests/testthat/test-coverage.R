111f99295376b5f1e96a674abc024559af1f1b72
Guillem Hurault <39564817+ghurault@users.noreply.github.com>
2019-10-04 12:46:02 +0100

    Update test coverage

--- tests/testthat/test-coverage.R
+++ tests/testthat/test-coverage.R
@@ -3,6 +3,8 @@ N_post <- 1e3
 truth <- rep(0, N)
 post_samples <- sapply(rnorm(N, 0, 1), function(x) {rnorm(N_post, x, 1)})
 
+# Somewhat redundant tests to those in test-stan_workflow but the input is more controlled here
+
 test_that("coverage is accurate", {
   cov_rmse <- with(compute_coverage(post_samples, truth),
        sqrt(mean((Nominal - Coverage)^2)))
--- tests/testthat/test-stan_workflow.R
+++ tests/testthat/test-stan_workflow.R
@@ -36,7 +36,7 @@ data_prior <- list(N = N,
                    N_repeat = N_repeat,
                    y = rep(1, N), # doesn't matter
                    run = 0)
-fit_prior <- sampling(compiled_model, data = data_prior)
+fit_prior <- sampling(compiled_model, data = data_prior, refresh = 0)
 
 # Check fit
 # check_hmc_diagnostics(fit_prior)
@@ -53,7 +53,7 @@ data_fake <- list(N = N,
                   N_repeat = N_repeat,
                   y = y_sim,
                   run = 1)
-fit_fake <- sampling(compiled_model, data = data_fake)
+fit_fake <- sampling(compiled_model, data = data_fake, refresh = 0)
 
 # Check fit
 # check_hmc_diagnostics(fit_fake)
@@ -61,16 +61,18 @@ fit_fake <- sampling(compiled_model, data = data_fake)
 
 # Tests -------------------------------------------------------------------
 
-test_that("summary_statistics returns a correct dataframe", {
-  par_prior <- summary_statistics(fit_prior, param)
-  par_fake <- summary_statistics(fit_fake, param)
+par_prior <- summary_statistics(fit_prior, param)
+par_fake <- summary_statistics(fit_fake, param)
 
+truth <- rstan::extract(fit_prior, pars = "mu")[[1]][draw, ]
+post_samples <- rstan::extract(fit_fake, pars = "mu")[[1]]
+
+test_that("summary_statistics returns a correct dataframe", {
   expect_equal(dim(par_fake), dim(par_prior))
   expect_equal(nrow(par_fake), N *length(param_obs) + N_subject * length(param_sub) + length(param_pop))
   expect_equal(nrow(par_fake[par_fake$Variable == "mu", ]), N_subject)
   expect_equal(nrow(par_fake[par_fake$Variable == "y_rep", ]), N)
 })
-
 
 test_that("process_replications works", {
   idx <- observations_dictionary(data_fake)
@@ -95,17 +97,20 @@ test_that("PPC_group_distribution returns a ggplot object", {
 })
 
 test_that("plot_prior_posterior returns a ggplot object", {
-  par_prior <- summary_statistics(fit_prior, param)
-  par_fake <- summary_statistics(fit_fake, param)
-
   expect_is(plot_prior_posterior(par_fake, par_prior, param_pop), "ggplot")
 })
 
 test_that("coverage is accurate", {
-  truth <- rstan::extract(fit_prior, pars = "mu")[[1]][draw, ]
-  post_samples <- rstan::extract(fit_fake, pars = "mu")[[1]]
-
   cov_rmse <- with(compute_coverage(post_samples, truth),
                    sqrt(mean((Nominal - Coverage)^2)))
-  expect_lt(cov_rmse, 0.1)
+  expect_lt(cov_rmse, 0.25) # high tolerance because of potential variability between runs
 })
+
+test_that("compute_coverage catch errors", {
+  expect_error(compute_coverage(post_samples, truth[-1]))
+})
+
+test_that("plot_coverage returns a ggplot object", {
+  expect_is(plot_coverage(post_samples, truth), "ggplot")
+})
+
