3db41510a217e7062c05235eb3f8b7ac9686ed4e
Guillem Hurault <39564817+ghurault@users.noreply.github.com>
2019-12-20 11:17:00 +0000

    Superficial changes

--- data-raw/compile_stan_model.R
+++ data-raw/compile_stan_model.R
@@ -1,7 +1,6 @@
 # Precompiled stan model
 # Can potentially compile model during build, but it still causes problem with the installed package size
 
-library(rstan)
-compiled_model <- stan_model(file = "inst/stan/hierarchical_model.stan")
+compiled_model <- rstan::stan_model(file = "inst/stan/hierarchical_model.stan", save_dso = TRUE)
 
 # saveRDS(compiled_model, file = "inst/testdata/hierarchical_compiled.rds")
