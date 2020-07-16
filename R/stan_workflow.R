2526b537e75f1cd74f111521ef4511921edf37bd
Guillem Hurault <guillem.hurault@hotmail.fr>
2020-07-16 21:30:32 +0100

    Enable multi-dimensional array in extract_draws

--- R/stan_workflow.R
+++ R/stan_workflow.R
@@ -228,12 +228,13 @@ process_replications <- function(fit, idx = NULL, parName, bounds = NULL, ...) {
 #'
 #' Not exported.
 #'
-#' @param obj Vector or matrix (columns represents different parameters) of draws
+#' @param obj Array/Vector/Matrix (first dimension corresponds to draws)
 #' @param draws Vector draws to extract
+#' @param parName Optional name to give to the parameter
 #'
-#' @return Dataframe with columns: Draw, Index, Value
+#' @return Dataframe with columns: Draw, Index (for 1d vector), Value, Parameter
 #' @noRd
-extract_draws_from_array <- function(obj, draws) {
+extract_draws_from_array <- function(obj, draws, parName = "") {
 
   stopifnot(is.vector(obj, mode = "numeric") || is.matrix(obj) || is.array(obj))
 
@@ -245,36 +246,50 @@ extract_draws_from_array <- function(obj, draws) {
 
   d <- dim(obj)
   if (length(d) == 1) {
-    data.frame(Draw = draws, Index = NA, Value = obj[draws])
+    out <- data.frame(Draw = draws, Index = NA, Value = obj[draws], Parameter = parName)
   } else if (length(d) == 2) {
+    # Faster implementation than code for length(d) > 2
     tmp <- obj[draws, ]
     if (length(draws) == 1) {
-      data.frame(Draw = draws, Index = 1:length(tmp), Value = tmp)
+      out <- data.frame(Draw = draws, Index = 1:length(tmp), Value = tmp)
     } else {
       rownames(tmp) <- draws
-      reshape2::melt(tmp,
+      out <- reshape2::melt(tmp,
                      varnames = c("Draw", "Index"),
                      value.name = "Value")
     }
+    out$Parameter <- parName
   } else {
-    stop("Parameters of more than one dimensions (e.g. matrix or array of array) are not supported yet")
+    id_lbl <- paste0("i", 1:(length(d) - 1))
+    out <- reshape2::melt(obj,
+                   varnames = c("Draw", id_lbl),
+                   value.name = "Value")
+    out <- out[out[["Draw"]] %in% draws, ]
+    lbl <- do.call(c, lapply(1:nrow(out), function(i) {paste0(out[i, id_lbl], collapse = ",")}))
+    lbl <- paste0(parName, "[", lbl, "]")
+    out$Index <- NA
+    out[, id_lbl] <- NULL
+    out$Parameter <- lbl
   }
+  return(out)
 }
 
 #' Extract parameters' draws
 #'
-#' @param obj Vector, matrix (columns represents different parameters) of draws or list of it
+#' @param obj Array/Vector/Matrix of draws (cf. first dimension) or list of it
 #' @param draws Vector of draws to extract
 #'
-#' @return Dataframe with columns: Draw, Index, Value and (only if input is a list) Parameter
+#' @return Dataframe with columns: Draw, Index, Value and Parameter
 #' @export
 #'
 #' @examples
 #' x <- rnorm(1e3)
 #' X <- matrix(x, ncol = 10)
+#' a <- array(rnorm(80), dim = c(10, 2, 2, 2))
 #' extract_draws(x, sample(1:length(x), 10))
 #' extract_draws(X, sample(1:nrow(X), 10))
-#' extract_draws(list(x = x, X = X), 1:10)
+#' extract_draws(a, sample(1:10, 5))
+#' extract_draws(list(x = x, X = X, a = a), 1:10)
 extract_draws <- function(obj, draws) {
 
   if (!(inherits(obj, "list") || is.vector(obj, mode = "numeric") || is.matrix(obj) || is.array(obj))) {
@@ -285,9 +300,7 @@ extract_draws <- function(obj, draws) {
     do.call(rbind,
             lapply(1:length(obj),
                    function(i) {
-                     tmp <- extract_draws_from_array(obj[[i]], draws)
-                     tmp[["Parameter"]] <- names(obj)[i]
-                     return(tmp)
+                     extract_draws_from_array(obj[[i]], draws, names(obj)[i])
                    }))
   } else {
     extract_draws_from_array(obj, draws)
--- tests/testthat/test-stan_workflow.R
+++ tests/testthat/test-stan_workflow.R
@@ -172,28 +172,35 @@ test_that("check_model_sensitivity catches error", {
 test_that("extract_draws and related function works", {
   x <- rnorm(1e3)
   X <- matrix(x, ncol = 10)
+  a1 <- array(rnorm(400), dim = c(100, 2, 2))
+  a2 <- array(rnorm(800), dim = c(100, 2, 2, 2))
 
   dr <- list(sample(1:length(x), 1),
              sample(1:length(x), 10),
              sample(1:nrow(X), 1),
              sample(1:nrow(X), 10),
-             11:20)
+             11:20,
+             21:30,
+             31:40)
   tmp <- list(extract_draws_from_array(x, dr[[1]]),
               extract_draws_from_array(x, dr[[2]]),
               extract_draws_from_array(X, dr[[3]]),
               extract_draws_from_array(X, dr[[4]]),
-              extract_draws(list(x = x, X = X), dr[[5]]))
+              extract_draws(list(x = x, X = X), dr[[5]]),
+              extract_draws_from_array(a1, dr[[6]]),
+              extract_draws_from_array(a2, dr[[7]]))
   sol_nrow <- c(length(dr[[1]]),
                 length(dr[[2]]),
                 length(dr[[3]]) * ncol(X),
                 length(dr[[4]]) * ncol(X),
-                length(dr[[5]]) * (1 + ncol(X)))
-  sol_ncol <- c(3, 3, 3, 3, 4)
+                length(dr[[5]]) * (1 + ncol(X)),
+                length(dr[[6]]) * 2 * 2,
+                length(dr[[7]]) * 2 * 2 * 2)
 
   for (i in 1:length(tmp)) {
     # Size dataframe
     expect_equal(nrow(tmp[[i]]), sol_nrow[i])
-    expect_equal(ncol(tmp[[i]]), sol_ncol[i])
+    expect_equal(ncol(tmp[[i]]), 4)
     # Content dataframe
     expect_equal(sort(unique(tmp[[i]][["Draw"]])), sort(dr[[i]]))
     expect_true(!any(is.na(tmp[[i]][["Value"]])))
