9d8c435fb70828a1de27755a23a9c2423de7a4f8
Guillem Hurault <guillem.hurault@hotmail.fr>
2020-07-20 16:44:02 +0100

    Deprecate extract_parameters_from_draw

--- R/deprec-stan_workflow.R
+++ R/deprec-stan_workflow.R
@@ -35,3 +35,38 @@ process_replications <- function(fit, idx = NULL, parName, bounds = NULL, ...) {
   }
   return(out)
 }
+
+#' Extract parameters from a single draw
+#'
+#' @param fit Stanfit object
+#' @param param Vector of parameter names
+#' @param draw Index of the draw to extract the parameters from
+#'
+#' @section Note:
+#' Useful for to generate fake data.
+#'
+#' @section Alternative:
+#' The tidybayes package offers an alternative to this function, for example:
+#'
+#' fit \%>\% tidy_draws() \%>\% gather_variables() \%>\% filter(.draw == draw & .variable \%in\% param)
+#'
+#' However, the tidybayes version is less efficient as all draws and parameters are extracted and then filtered (also the draw IDs are not the same).
+#' Using tidybayes would be more recommended when we only want to extract specific parameters, and that it does not matter which draw are extracted (in that case using spread_draws).
+#'
+#' @return Dataframe
+#' @export
+extract_parameters_from_draw <- function(fit, param, draw) {
+
+  .Deprecated("extract_draws")
+
+  stopifnot(is_stanfit(fit))
+
+  draw <- as.integer(draw)
+  if (length(draw) != 1) {
+    warning(as.character(draw), " should be a single number, taking the first element")
+    draw <- draw[1]
+  }
+
+  par <- rstan::extract(fit, pars = param)
+  extract_draws(par, draw)
+}
