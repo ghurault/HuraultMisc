7866ee59271c9afd43e183c64ff0ceca3c87f388
Guillem Hurault <guillem.hurault@hotmail.fr>
2021-01-21 10:55:23 +0000

    Use pipe

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,3 +1,17 @@
+# Pipe --------------------------------------------------------------------
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
 # Predicates --------------------------------------------------------------
 
 #' Test whether an object is of class "stanfit"
